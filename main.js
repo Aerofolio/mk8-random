c860833b938acf7b0c7f0a446c25d1dbd342ec6e
Paulo Bodaneze Reva <paulo.reva@metadados.com.br>
2023-04-11 15:30:41 -0300

    finishing vechicles tables

--- main.js
+++ main.js
@@ -299,18 +299,284 @@ const vehicles = [
         type: 'kart'
     },
     {
-        imagePath: 'images/vehicles/karts/pipeFrameKart.webp',
+        imagePath: 'images/vehicles/karts/pipeFrameKart.png',
         name: 'Pipe Frame',
         id: 2,
         selectable: true,
         type: 'kart'
     },
     {
-        imagePath: 'images/vehicles/karts/mach8Kart.webp',
+        imagePath: 'images/vehicles/karts/mach8Kart.png',
         name: 'Mach 8',
         id: 3,
         selectable: true,
         type: 'kart'
+    },
+    {
+        imagePath: 'images/vehicles/karts/steelDriverKart.png',
+        name: 'Steel Driver',
+        id: 4,
+        selectable: true,
+        type: 'kart'
+    },
+    {
+        imagePath: 'images/vehicles/karts/catCruiserKart.png',
+        name: 'Cat Cruiser',
+        id: 5,
+        selectable: true,
+        type: 'kart'
+    },
+    {
+        imagePath: 'images/vehicles/karts/circuitSpecialKart.png',
+        name: 'Circuit Special',
+        id: 6,
+        selectable: true,
+        type: 'kart'
+    },
+    {
+        imagePath: 'images/vehicles/karts/triSpeederKart.png',
+        name: 'Tri-Speeder',
+        id: 7,
+        selectable: true,
+        type: 'kart'
+    },
+    {
+        imagePath: 'images/vehicles/karts/badWagonKart.png',
+        name: 'Bad Wagon',
+        id: 8,
+        selectable: true,
+        type: 'kart'
+    },
+    {
+        imagePath: 'images/vehicles/karts/PrancerKart.png',
+        name: 'Prancer',
+        id: 9,
+        selectable: true,
+        type: 'kart'
+    },
+    {
+        imagePath: 'images/vehicles/karts/biddybuggyKart.png',
+        name: 'Biddybuggy',
+        id: 10,
+        selectable: true,
+        type: 'kart'
+    },
+    {
+        imagePath: 'images/vehicles/karts/landshipKart.png',
+        name: 'Landship',
+        id: 11,
+        selectable: true,
+        type: 'kart'
+    },
+    {
+        imagePath: 'images/vehicles/karts/sneekerKart.png',
+        name: 'Sneeker',
+        id: 12,
+        selectable: true,
+        type: 'kart'
+    },
+    {
+        imagePath: 'images/vehicles/karts/sportsCoupeKart.png',
+        name: 'Sports Coupe',
+        id: 13,
+        selectable: true,
+        type: 'kart'
+    },
+    {
+        imagePath: 'images/vehicles/karts/goldStandartKart.png',
+        name: 'Gold Standart',
+        id: 14,
+        selectable: true,
+        type: 'kart'
+    },
+    {
+        imagePath: 'images/vehicles/karts/glaKart.png',
+        name: 'GLA',
+        id: 15,
+        selectable: true,
+        type: 'kart'
+    },
+    {
+        imagePath: 'images/vehicles/karts/w25SilverArrowKart.png',
+        name: 'W 25 Silver Arrow',
+        id: 16,
+        selectable: true,
+        type: 'kart'
+    },
+    {
+        imagePath: 'images/vehicles/karts/300SLRoadsterKart.png',
+        name: '300 SL Roadster',
+        id: 17,
+        selectable: true,
+        type: 'kart'
+    },
+    {
+        imagePath: 'images/vehicles/karts/blueFalconKart.png',
+        name: 'Blue Falcon',
+        id: 18,
+        selectable: true,
+        type: 'kart'
+    },
+    {
+        imagePath: 'images/vehicles/karts/tanookiKartKart.png',
+        name: 'Tanooki Kart',
+        id: 19,
+        selectable: true,
+        type: 'kart'
+    },
+    {
+        imagePath: 'images/vehicles/karts/bDasherKart.png',
+        name: 'B Dasher',
+        id: 20,
+        selectable: true,
+        type: 'kart'
+    },
+    {
+        imagePath: 'images/vehicles/karts/streetleKart.png',
+        name: 'Steetle',
+        id: 21,
+        selectable: true,
+        type: 'kart'
+    },
+    {
+        imagePath: 'images/vehicles/karts/pWingKart.png',
+        name: 'P Wing',
+        id: 22,
+        selectable: true,
+        type: 'kart'
+    },
+    {
+        imagePath: 'images/vehicles/karts/koopaClownKart.png',
+        name: 'Koopa Clown',
+        id: 23,
+        selectable: true,
+        type: 'kart'
+    },
+    {
+        imagePath: 'images/vehicles/standartBikes/standartBikeBike.png',
+        name: 'Standart Bike',
+        id: 24,
+        selectable: true,
+        type: 'standartBike'
+    },
+    {
+        imagePath: 'images/vehicles/standartBikes/theDukeBike.png',
+        name: 'The Duke',
+        id: 25,
+        selectable: true,
+        type: 'standartBike'
+    },
+    {
+        imagePath: 'images/vehicles/standartBikes/flameRiderBike.png',
+        name: 'Flame Rider',
+        id: 26,
+        selectable: true,
+        type: 'standartBike'
+    },
+    {
+        imagePath: 'images/vehicles/standartBikes/varmitBike.png',
+        name: 'Varmit',
+        id: 27,
+        selectable: true,
+        type: 'standartBike'
+    },
+    {
+        imagePath: 'images/vehicles/standartBikes/mrScootyBike.png',
+        name: 'Mr. Scooty',
+        id: 28,
+        selectable: true,
+        type: 'standartBike'
+    },
+    {
+        imagePath: 'images/vehicles/standartBikes/masterCycleZeroBike.png',
+        name: 'Master Cycle Zero',
+        id: 29,
+        selectable: true,
+        type: 'standartBike'
+    },
+    {
+        imagePath: 'images/vehicles/standartBikes/CityTripperBike.png',
+        name: 'City Tripper',
+        id: 30,
+        selectable: true,
+        type: 'standartBike'
+    },
+    {
+        imagePath: 'images/vehicles/sportBikes/cometBike.png',
+        name: 'Comet',
+        id: 31,
+        selectable: true,
+        type: 'sportBike'
+    },
+    {
+        imagePath: 'images/vehicles/sportBikes/sportBikeBike.png',
+        name: 'Sport Bike',
+        id: 32,
+        selectable: true,
+        type: 'sportBike'
+    },
+    {
+        imagePath: 'images/vehicles/sportBikes/jetBikeBike.png',
+        name: 'Jet Bike',
+        id: 33,
+        selectable: true,
+        type: 'sportBike'
+    },
+    {
+        imagePath: 'images/vehicles/sportBikes/yoshiBikeBike.png',
+        name: 'Yoshi Bike',
+        id: 34,
+        selectable: true,
+        type: 'sportBike'
+    },
+    {
+        imagePath: 'images/vehicles/sportBikes/masterCycleBike.png',
+        name: 'Master Cycle',
+        id: 35,
+        selectable: true,
+        type: 'sportBike'
+    },
+    {
+        imagePath: 'images/vehicles/atvs/standartATVATV.png',
+        name: 'Standart ATV',
+        id: 36,
+        selectable: true,
+        type: 'atv'
+    },
+    {
+        imagePath: 'images/vehicles/atvs/wildWigglerATV.png',
+        name: 'wild Wiggler',
+        id: 37,
+        selectable: true,
+        type: 'atv'
+    },
+    {
+        imagePath: 'images/vehicles/atvs/teddyBuggyATV.png',
+        name: 'Teddy Buggy',
+        id: 38,
+        selectable: true,
+        type: 'atv'
+    },
+    {
+        imagePath: 'images/vehicles/atvs/boneRattlerATV.png',
+        name: 'Bone Rattler',
+        id: 39,
+        selectable: true,
+        type: 'atv'
+    },
+    {
+        imagePath: 'images/vehicles/atvs/splattBuggyATV.png',
+        name: 'Splat Buggy',
+        id: 40,
+        selectable: true,
+        type: 'atv'
+    },
+    {
+        imagePath: 'images/vehicles/atvs/inkstrikerATV.png',
+        name: 'Inkstriker',
+        id: 41,
+        selectable: true,
+        type: 'atv'
     }
 ]
 
@@ -321,26 +587,34 @@ selectVehicle = (vehicleId) => {
 }
 
 createVehiclesTables = () => {
-    let vehiclesTableInnerHTML = '';
+    document.getElementById('kartsTable').innerHTML = createVehicleTableHTML(vehicles.filter(x => x.type == 'kart'));
+    document.getElementById('standartBikesTable').innerHTML = createVehicleTableHTML(vehicles.filter(x => x.type == 'standartBike'));
+    document.getElementById('sportBikesTable').innerHTML = createVehicleTableHTML(vehicles.filter(x => x.type == 'sportBike'));
+    document.getElementById('atvsTable').innerHTML = createVehicleTableHTML(vehicles.filter(x => x.type == 'atv'));
+}
+
+createVehicleTableHTML = (vehiclesArray) => {
+    let vehicleTableInnerHTML = '';
     let column = 0;
-    for(let i = 0; i < vehicles.length; i++){
-        if(column == 0) charactersTableInnerHTML += '<tr>';
+
+    for(let i = 0; i < vehiclesArray.length; i++){
+        if(column == 0) vehicleTableInnerHTML += '<tr>';
     
-        let character = characters[i];
-        charactersTableInnerHTML += '<td id="' + character.name + '-' + character.id + '" class="selectableCell" onclick="selectCharacter(' + character.id + ')">';
-        charactersTableInnerHTML += '<span>' + character.name + '</span>';
-        charactersTableInnerHTML += '<br>';
-        charactersTableInnerHTML += '<img class="charactersIcon" src="' + character.imagePath + '">';
-        charactersTableInnerHTML += '</td>';
+        let vehicle = vehiclesArray[i];
+        vehicleTableInnerHTML += '<td id="' + vehicle.name + '-' + vehicle.id + '" class="selectableCell" onclick="selectVehicle(' + vehicle.id + ')">';
+        vehicleTableInnerHTML += '<span>' + vehicle.name + '</span>';
+        vehicleTableInnerHTML += '<br>';
+        vehicleTableInnerHTML += '<img class="vehiclePart" src="' + vehicle.imagePath + '">';
+        vehicleTableInnerHTML += '</td>';
     
         column++;
         if(column == numberOfColumns){
-            charactersTableInnerHTML += '</tr>';
+            vehicleTableInnerHTML += '</tr>';
             column = 0;
         }
     }
-    
-    document.getElementById('charactersTable').innerHTML = charactersTableInnerHTML;
+
+    return vehicleTableInnerHTML;
 }
 
 //################## Util ##################
